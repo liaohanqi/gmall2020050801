6e133b05cab8ab82351aecef2302b6d2466fe631
liaohanqi <1328934693@qq.com>
2020-05-11 17:17:09 +0800

    day04第一次提交

--- gmall-manager-service/src/main/java/com/liaohanqi/gmall/manager/mapper/PmsProductSaleAttrMapper.java
+++ gmall-manager-service/src/main/java/com/liaohanqi/gmall/manager/mapper/PmsProductSaleAttrMapper.java
@@ -1,8 +1,12 @@
 package com.liaohanqi.gmall.manager.mapper;
 
 import com.liaohanqi.gmall.bean.PmsProductSaleAttr;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 public interface PmsProductSaleAttrMapper extends Mapper<PmsProductSaleAttr> {
 
+    List<PmsProductSaleAttr> spuSaleAttrListBysql(@Param("spuId") String spuId, @Param("skuId") String skuId);
 }
